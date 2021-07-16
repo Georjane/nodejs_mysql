2f33300a8546e799252c76f4ef239a66f9743ee1
Georjane <pzgeorjane@gmail.com>
2021-07-16 06:29:53 +0100

    Add mysql code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "posgres",
+  password: "15oct17jun",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  
+});
